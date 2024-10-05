34581f41c52f4366844ee1fe939483921f7bb212
DylanBarboza <barbozadylan44@gmail.com>
2024-10-05 01:28:44 +0000

    Cambios..

--- RTA_SCRIPT_Examen_20241004/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo mkdir -p /Examenes-UTN/{alumno_1/{alumno_1,alumno_2,alumno_3},alumno_2/{alumno_1,alumno_2,alumno_3},alumno_3/{alumno_1,alumno_2,alumno_3},profesores} 
+sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores} 
 
 
 echo "-------------------------"
