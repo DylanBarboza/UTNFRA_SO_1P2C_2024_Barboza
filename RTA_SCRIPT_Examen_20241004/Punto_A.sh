9590887bc21f294159ea06790e5b37d7492e5d13
DylanBarboza <barbozadylan44@gmail.com>
2024-10-05 00:40:10 +0000

    Tu mensaje de commit

---
+++ RTA_SCRIPT_Examen_20241004/Punto_A.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sudo mkdir -p /Examenes-UTN/{alumno_1/{alumno_1,alumno_2,alumno_3},alumno_2/{alumno_1,alumno_2,alumno_3},alumno_3/{alumno_1,alumno_2,alumno_3},profesores} 
+
+
+echo "-------------------------"
+echo "----CarpetasCreadas<3----"
+echo "-------------------------"
