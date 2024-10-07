f33190e7a8c364a736a94fb32ade80ce5750df14
DylanBarboza <barbozadylan44@gmail.com>
2024-10-07 18:35:48 +0000

    Punto C Finalizado

--- RTA_SCRIPT_Examen_20241004/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_C.sh
@@ -19,25 +19,27 @@ sudo usermod -aG p1c2_2024_gAlumno p1c2_2024_A2
 sudo usermod -aG p1c2_2024_gAlumno p1c2_2024_A3
 sudo usermod -aG p1c2_2024_gProfesores p1c2_2024_P1
 
-#MUESTRO LAS CLAVES QUE SEAN IGUALES
-sudo grep -E 'vagrant|p1c2_2024_A1|p1c2_2024_A2|p1c2_2024_gAlumno p1c2_2024_A3|p1c2_2024_P1' /etc/shadow |awk -F ':' '{print $2}'
+#Le doy permiso de DUEÑO a cada Alumno/Profesor
+sudo chown p1c2_2024_A1  /Examenes-UTN/alumno_1
+sudo chown p1c2_2024_A2  /Examenes-UTN/alumno_2
+sudo chown p1c2_2024_A3  /Examenes-UTN/alumno_3
+sudo chown p1c2_2024_P1  /Examenes-UTN/profesores
+
 
 #PERMISOS A CADA CARPETA CON SU USUARIO
-sudo chmod 730 /Examenes-UTN/alumno_1
+sudo chmod 750 /Examenes-UTN/alumno_1
 sudo chmod 760 /Examenes-UTN/alumno_2
 sudo chmod 700 /Examenes-UTN/alumno_3
-sudo chmod 773 /Examenes-UTN/profesores
+sudo chmod 775 /Examenes-UTN/profesores
+
+#Que cada usuario cree su propia carpeta.
+sudo su -c "whoami" p1c2_2024_A1 | sudo tee /Examenes-UTN/alumno_1/validar1.txt
+sudo su -c "whoami" p1c2_2024_A2 | sudo tee /Examenes-UTN/alumno_2/validar2.txt
+sudo su -c "whoami" p1c2_2024_A3 | sudo tee /Examenes-UTN/alumno_3/validar3.txt
+sudo su -c "whoami" p1c2_2024_P1 | sudo tee /Examenes-UTN/profesores/profesores.txt
 
 echo "--------------------------"
 echo "----USUARIOS-CREADOS------"
 echo "-----GRUPOS-CREADOS-------"
 echo "--------PASSWORD----------"
 echo "--------------------------"
-
-#MUESTRO MIS USUARIOS CREADOS
-echo "---------Usuarios--------"
-cut -d: -f1 /etc/passwd | tail -n 4
-echo "-------------------------"
-
-
-
