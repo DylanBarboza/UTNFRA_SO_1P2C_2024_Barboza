c9b1c27218a5e7914882489b02d6af81d8293912
DylanBarboza <barbozadylan44@gmail.com>
2024-10-07 17:03:50 +0000

    Punto E Finalizado

---
+++ RTA_SCRIPT_Examen_20241004/Punto_E.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Busco/Filtro la primera linea de memoria
+
+echo "$(head -n 1 /proc/meminfo)" > ~/repogit/UTNFRA_SO_1P2C_2024_Barboza/RTA_ARCHIVOS_Examen_20241007/Filtro_Basico.txt
+echo "Informacion Chassis : $(sudo dmidecode -t chassis | grep 'Manufacturer:' | awk '{print $2, $3}')" >>  ~/repogit/UTNFRA_SO_1P2C_2024_Barboza/RTA_ARCHIVOS_Examen_20241007/Filtro_Basico.txt
+
+
+echo "-----------------------"
+echo "------TXT-CREADO-------"
+echo "----FILTROS BASICOS----"
+echo "-----------------------"
+
+
