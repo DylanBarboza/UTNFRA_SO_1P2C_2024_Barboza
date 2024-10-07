aa5c5afa3105fa0b24f709c62fccf54f870b47f0
DylanBarboza <barbozadylan44@gmail.com>
2024-10-07 17:05:58 +0000

    Punto D Finalizado

--- RTA_SCRIPT_Examen_20241004/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_D.sh
@@ -2,3 +2,8 @@
 
 #Creo la carpeta
 mkdir -p /home/Estructura_Asimetrica/{correo/{cartas{1..100},carterps{1..10}},clientes/cartas{1..100}}
+
+echo "--------------------"
+echo "--Carpetas-Creadas--"
+echo "--------------------"
+
